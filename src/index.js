da338b483d90e810f9d2f8e98ae7031e0966f00a
Eiichi599 <imgabe0408@gmail.com>
2025-07-08 01:54:04 -0300

    atualizado

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Hello world")
+let nome = prompt("Qual seu nome?")
+let xp = parseInt(prompt("Qual sua quantidade de xp ?"))
+rank = "";
+if (xp < 1000){
+    rank = "Ferro";
+}else if (xp <= 2000){
+    rank = "Bronze"
+}else if (xp <= 5000){
+    rank = "Prata"
+}else if (xp <= 7000){
+    rank = "Ouro"
+}else if (xp <= 8000){
+    rank = "Platina"
+}else if (xp <= 9000){
+    rank = "Ascendente"
+}else if (xp <= 10000){
+    rank = "Imortal"
+}else {
+    rank = "Radiante"
+}
+
+console.log(`O heroi de nome ${nome} esta no nivel de ${rank}`)
